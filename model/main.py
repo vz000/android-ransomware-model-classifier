811f0f835627e55adf9e56d6b55ba324755db22d
vz000 <vrosasz1700@alumno.ipn.mx>
2023-06-03 18:25:45 -0600

    Add path for rust
    Adds missing parameter for rust function

--- model/main.py
+++ model/main.py
@@ -7,7 +7,7 @@ def launch(apk_path: str) -> dict:
     parse_static_data(apk_path)
     classify = static_classify('./out/permissions.csv',3)
     static_results = classify.classify()
-    rust.start_analysis()
+    rust.start_analysis(apk_path)
     get_freq_list = [parse_freq_data("ransomware"),parse_freq_data("goodware")]
     parsed_out = parse_dynamic_data()
     dynamic_result = dynamic_classify(parsed_out.parsed_logs(), get_freq_list)
